2ed292bdd5f6200311263215dcb3bafb5d6e74ce
Musandiwa <musasinyoc@gmail.com>
2023-06-26 03:46:03 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * reset_to_98 - Updates the value it points to 98
@@ -8,5 +8,11 @@
   */
 void reset_to_98(int *n)
 {
-	*n = 98;
+        int n;
+
+        n = 402;
+        printf("n=%d\n", n);
+        reset_to_98(&n);
+        printf("n=%d\n", n);
+        return (0);
 }
